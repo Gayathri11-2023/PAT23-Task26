c9183f7d0ad37578f2dbda58924e87b268499617
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-03-10 17:41:18 +0530

    locator.py uploaded

---
+++ locator.py
@@ -0,0 +1,22 @@
+class WebLocators:
+    def __init__(self):
+        self.expandLocator = "//button[@data-testid='adv-search-expand-all']"
+        self.nameLocator = "text-input__3"
+        self.birthFromDateLocator = "text-input__8"
+        self.seeResultsLocator = "//button[@data-testid='adv-search-get-results']"
+        '''
+        self.birthFromDateLocator = "//input[@id='text-input__8']"
+        self.birthToDateLocator = "//input[@id='text-input__9']"
+        self.birthdayLocator = "//input[@id='text-input__4']"
+        self.awardsLocator = "//button[@data-testid='test-chip-id-oscar_best_actress_nominees']"
+        self.pageTopicLocator = "//button[@data-testid='test-chip-id-BIOGRAPHY']"
+        self.selectDropdownLocator = "//select[@id='within-topic-dropdown-id']"
+        self.chooseValueLocator = "//option[@value='BIOGRAPHY']"
+        self.subTopicLocator = "//input[@id='text-input__5']"
+        self.deathFromDateLocator = "//input[@id='text-input__12']"
+        self.deathToDateLocator = "//input[@id='text-input__13']"
+        self.genderLocator = "//button[@data-testid='test-chip-id-FEMALE']"
+        self.creditsLocator = "// input[ @ data - testid = 'autosuggest-input-test-id-filmography']"
+        self.adultLocator = "//input[@data-testid='include-adult-names']"
+
+      '''
